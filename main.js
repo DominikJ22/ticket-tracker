0d11692038cd0716def332ed98aa31b3a4471272
Dominik Jones <dominikjo22@gmail.com>
2022-11-17 14:06:01 -0500

    building functions

---
+++ main.js
@@ -0,0 +1,46 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue)
+
+
+
+function fetchIssues(){
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    let issuesList = document.getElementById('issuesList')
+
+
+    issuesList.innerHTML = '';
+
+    for (let i = 0; i < issues.length; i++){
+        let id = issues[i].id
+        let subject = issues[i].subject
+        let description = issues[i].description
+        let severity = issues[i].severity
+        let assignedTo = issues[i].assignedTo
+        let status = issues[i].status
+        let statusColor = status == "Closed" ? 'label-success' : 'label-info'
+        
+       // issuesList.innerHTML += 
+    }
+
+}
+
+function saveIssue(e){
+    let issueId = chance.guid()
+    let issueSubject = document.getElementById('issueSubjInput').value
+    let issueDescription = document.getElementById('issueDescInput').value
+    let issueSeverity = document.getElementById('issueSeverityInput').value
+    let issueAssigned = document.getElementById('issueAssignedInput').value
+    let issueStatus = 'Open'
+
+    let issues = {
+        id: issueId,
+        subject: issueSubject,
+        description: issueDescription,
+        severity: issueSeverity,
+        assigned: issueAssigned,
+        status: issueStatus
+
+    }
+
+    
+
+}
